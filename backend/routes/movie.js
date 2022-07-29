0ba754994d15274b7a2f3d0460f3e330e788526a
Hamza Ben Abdallah <hamza.ben.abdallah02@gmail.com>
2022-07-28 21:40:58 -0400

    optimization

--- backend/routes/movie.js
+++ backend/routes/movie.js
@@ -4,33 +4,37 @@ dotenv.config();
 
 const { API_KEY, MOVIE_BASE_URL } = process.env;
 
-export const getCurrentlyPlayingMovies = async (req, res) => {
+const baseGetMovie = async (req, res, urlParam) => {
   const { page, region } = req.query;
-  const url = `${MOVIE_BASE_URL}/movie/now_playing?api_key=${API_KEY}&page=${
+  const url = `${MOVIE_BASE_URL}${urlParam}?api_key=${API_KEY}&page=${
     page || 1
   }&region=${region || "US"}`;
-
   try {
     const response = await axios(url);
     const data = response?.data.results;
 
-    const relevantData = data.map((movie) => ({
-      id: movie.id,
-      title: movie.title,
-      overview: movie.overview,
-      vote_average: movie.vote_average,
-      vote_count: movie.vote_count,
-      release_date: movie.release_date,
-      poster_path: movie.poster_path,
-      genre_id: movie.genre_ids,
-    }));
-    res.status(200).json(relevantData);
+    if (data.length > 0) {
+      const relevantData = data.map((movie) => ({
+        id: movie.id,
+        title: movie.title,
+        overview: movie.overview,
+        vote_average: movie.vote_average,
+        vote_count: movie.vote_count,
+        release_date: movie.release_date,
+        poster_path: movie.poster_path,
+        genre_id: movie.genre_ids,
+      }));
+      res.status(200).json(relevantData);
+    } else {
+      res.status(404).json({ status: 404, message: "No movies found" });
+    }
   } catch (err) {
     switch (err.response.status) {
+      case 400:
       case 422:
         res
-          .status(422)
-          .json({ status: 422, message: "Please enter a valid page number" });
+          .status(404)
+          .json({ status: 404, message: "Please enter a valid page number" });
         break;
       default:
         res.status(500).json({
@@ -39,78 +43,19 @@ export const getCurrentlyPlayingMovies = async (req, res) => {
         });
     }
   }
+};
+
+export const getCurrentlyPlayingMovies = async (req, res) => {
+  const urlParam = `/movie/now_playing`;
+  baseGetMovie(req, res, urlParam);
 };
 
 export const getTopRatedMovies = async (req, res) => {
-  const { page, region } = req.query;
-  const url = `${MOVIE_BASE_URL}/movie/top_rated?api_key=${API_KEY}&page=${
-    page || 1
-  }&region=${region || "US"}`;
-
-  try {
-    const response = await axios(url);
-    const data = response?.data.results;
-
-    const relevantData = data.map((movie) => ({
-      id: movie.id,
-      title: movie.title,
-      overview: movie.overview,
-      vote_average: movie.vote_average,
-      vote_count: movie.vote_count,
-      release_date: movie.release_date,
-      poster_path: movie.poster_path,
-      genre_id: movie.genre_ids,
-    }));
-    res.status(200).json(relevantData);
-  } catch (err) {
-    switch (err.response.status) {
-      case 422:
-        res
-          .status(422)
-          .json({ status: 422, message: "Please enter a valid page number" });
-        break;
-      default:
-        res.status(500).json({
-          status: 500,
-          message: err.message,
-        });
-    }
-  }
+  const urlParam = `/movie/top_rated`;
+  baseGetMovie(req, res, urlParam);
 };
 
 export const getTrendingMovies = async (req, res) => {
-  const { page, region } = req.query;
-  const url = `${MOVIE_BASE_URL}/trending/movie/week?api_key=${API_KEY}&page=${
-    page || 1
-  }&region=${region || "US"}`;
-
-  try {
-    const response = await axios(url);
-    const data = response?.data.results;
-
-    const relevantData = data.map((movie) => ({
-      id: movie.id,
-      title: movie.title,
-      overview: movie.overview,
-      vote_average: movie.vote_average,
-      vote_count: movie.vote_count,
-      release_date: movie.release_date,
-      poster_path: movie.poster_path,
-      genre_id: movie.genre_ids,
-    }));
-    res.status(200).json(relevantData);
-  } catch (err) {
-    switch (err.response.status) {
-      case 422:
-        res
-          .status(422)
-          .json({ status: 422, message: "Please enter a valid page number" });
-        break;
-      default:
-        res.status(500).json({
-          status: 500,
-          message: err.message,
-        });
-    }
-  }
+  const urlParam = `/trending/movie/week`;
+  baseGetMovie(req, res, urlParam);
 };
