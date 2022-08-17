650acc05ce38d558c78e8c5f87e3f7846fd06b12
Hamza <hamza.ben.abdallah02@gmail.com>
2022-08-17 16:20:17 -0400

    refactor movie details endpoint

--- server/routes/movies/details.js
+++ server/routes/movies/details.js
@@ -1,10 +1,8 @@
-import { MongoClient } from "mongodb";
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-const { API_KEY, MOVIE_BASE_URL, MONGO_URI } = process.env;
-const client = new MongoClient(MONGO_URI);
+const { API_KEY, MOVIE_BASE_URL } = process.env;
 
 export const getDetails = async (req, res) => {
   const { id } = req.query;
@@ -13,48 +11,34 @@ export const getDetails = async (req, res) => {
   const castUrl = `${MOVIE_BASE_URL}/movie/${id}/credits?api_key=${API_KEY}`;
 
   try {
-    // Connect to the database
-    await client.connect();
+    const response = await axios(url);
 
-    try {
-      const response = await axios(url);
-      const { data } = response;
-      const details = { ...data, watchlist: false, watched: false };
+    const similar = await axios(similarUrl);
+    const { data: similarData } = similar;
 
-      const similar = await axios(similarUrl);
-      const { data: similarData } = similar;
-      const cast = await axios(castUrl);
-      const { data: castData } = cast;
-      const director = castData.crew.find(
-        (member) => member.job === "Director"
-      );
-      const returnData = {
-        ...details,
-        related: similarData.results.slice(0, 5),
-        cast: castData.cast.slice(0, 4),
-        director: director ? director.name : "",
-      };
+    const cast = await axios(castUrl);
+    const { data: castData } = cast;
 
-      if (data) {
-        res.status(200).json(returnData);
-      } else {
-        res.status(404).json({
-          status: 404,
-          message: "No results found, please enter a valid genre id",
-        });
-      }
-    } catch (err) {
-      res.status(500).json({
-        status: 500,
-        message: `Internal server error`,
+    const director = castData.crew.find((member) => member.job === "Director");
+    const returnData = {
+      ...response.data,
+      related: similarData.results.slice(0, 5),
+      cast: castData.cast.slice(0, 4),
+      director: director ? director.name : "",
+    };
+
+    if (response.data) {
+      res.status(200).json(returnData);
+    } else {
+      res.status(404).json({
+        status: 404,
+        message: "No results found, please enter a valid genre id",
       });
     }
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({
       status: 500,
-      message: "Internal server error",
+      message: `Internal server error`,
     });
-  } finally {
-    await client.close();
   }
 };
