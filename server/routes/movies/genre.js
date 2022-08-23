5e8254236440770dfee2ac579e343021b1ae98ac
Hamza <hamza.ben.abdallah02@gmail.com>
2022-08-22 21:53:15 -0400

    Modify the genre endpoint

--- server/routes/movies/genre.js
+++ server/routes/movies/genre.js
@@ -5,28 +5,11 @@ dotenv.config();
 const { API_KEY, MOVIE_BASE_URL } = process.env;
 
 export const getGenre = async (req, res) => {
-  const { id = 28 } = req.query;
-  const url = `${MOVIE_BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`;
+  const url = `${MOVIE_BASE_URL}/genre/movie/list?api_key=${API_KEY}`;
 
   try {
-    const response = await axios(url);
-    const data = response.data.genres.find((genre) => {
-      if (genre.id === Number(id)) {
-        return genre;
-      }
-    });
-    const genre = data?.name;
-    if (genre) {
-      res.status(200).json({
-        status: 200,
-        genre: `${genre}`,
-      });
-    } else {
-      res.status(404).json({
-        status: 404,
-        message: "No results found, please enter a valid genre id",
-      });
-    }
+    const { data } = await axios(url);
+    res.status(200).json(data.genres);
   } catch (err) {
     res.status(500).json({
       status: 500,
