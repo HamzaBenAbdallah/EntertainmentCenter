8296651fea766cb52316ddf4009b94b93bf62e6d
Hamza <hamza.ben.abdallah02@gmail.com>
2022-08-27 19:35:16 -0400

    Style the Header buttons

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -2,44 +2,57 @@ import { useLogout } from "./useLogout";
 import { getCurrentUser } from "Services/getCurrentUser";
 import SearchBar from "Components/SearchBar";
 import { MdOutlineAccountCircle } from "react-icons/md";
-import { Wrapper, Container, Title, Page, Link, Icon } from "./Header.style";
+import {
+  Head,
+  Wrapper,
+  Container,
+  Title,
+  Button,
+  Page,
+  Link,
+  Icon,
+} from "./Header.style";
 
 const Header = () => {
   const { user } = getCurrentUser();
   const { handleLogout } = useLogout();
   return (
-    <Wrapper>
-      <Container>
-        <Link to="">
-          <Title>Movies</Title>
-        </Link>
-        <Link to="Discover">
-          <Page>Discover</Page>
-        </Link>
-      </Container>
-      <Container>
-        <SearchBar />
-        {!user ? (
-          <>
-            <Link to="login">
-              <Page>Login</Page>
-            </Link>
-            <Link to="signup">
-              <Page>Signup</Page>
-            </Link>
-          </>
-        ) : (
-          <Link to="" onClick={handleLogout}>
-            <Page>Logout</Page>
+    <Head>
+      <Wrapper>
+        <Container>
+          <Link to="">
+            <Title>Movies</Title>
           </Link>
-        )}
-        <Icon>
-          <Link to="profile">
-            <MdOutlineAccountCircle size="2em" />
+          <Link to="Discover">
+            <Page>Discover</Page>
           </Link>
-        </Icon>
-      </Container>
-    </Wrapper>
+        </Container>
+        <Container>
+          <SearchBar />
+          {!user ? (
+            <>
+              <Link to="login">
+                <Button>Login</Button>
+              </Link>
+              <Link to="signup">
+                <Button>Signup</Button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="" onClick={handleLogout}>
+                <Button>Logout</Button>
+              </Link>
+              <Icon>
+                <Link to="profile">
+                  <MdOutlineAccountCircle size="2em" />
+                </Link>
+              </Icon>
+            </>
+          )}
+        </Container>
+      </Wrapper>
+    </Head>
   );
 };
 
--- client/src/Components/Header/Header.style.js
+++ client/src/Components/Header/Header.style.js
@@ -1,27 +1,46 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
+export const Head = styled.header`
+  display: flex;
+  justify-content: center;
+  background-color: #032541;
+  border-bottom: 2px solid #000;
+  height: 5rem;
+`;
+
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  border-bottom: 2px solid #000;
-  background-color: #032541;
   color: white;
+  width: 80%;
 `;
 
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  gap: 2rem;
+  gap: 1rem;
 `;
 
 export const Title = styled.div`
   margin-left: 4rem;
   font-size: 2.5em;
 `;
+
 export const Page = styled.div`
   font-size: 1.25em;
+`;
+
+export const Button = styled.div`
+  font-size: 1.1em;
+  border: 2px solid #abb7c4;
+  padding: 6px 15px;
+  border-radius: 30px;
+
+  &:hover {
+    border-color: #fff;
+  }
 `;
 
 export const Link = styled(NavLink)`
