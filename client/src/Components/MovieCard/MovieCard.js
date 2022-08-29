42ceb15df72dad6dd975ff89afe6f2855311778d
Hamza <hamza.ben.abdallah02@gmail.com>
2022-08-29 19:30:35 -0400

    Refactor image error

--- client/src/Components/MovieCard/MovieCard.js
+++ client/src/Components/MovieCard/MovieCard.js
@@ -4,14 +4,19 @@ import NotFound from "Pictures/Image-not-found.jpg";
 
 const MovieCard = ({ movie }) => {
   const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original";
-  const image = movie?.poster_path
-    ? `${BASE_IMAGE_URL}${movie.poster_path}`
-    : NotFound;
+
+  const handleError = (e) => {
+    e.target.src = NotFound;
+  };
 
   return (
     <Container>
       <Link to={`/movies/${movie?.id}`}>
-        <Poster src={image} alt={movie?.title} />
+        <Poster
+          src={`${BASE_IMAGE_URL}${movie.poster_path}`}
+          alt={movie?.title}
+          onError={handleError}
+        />
       </Link>
     </Container>
   );
