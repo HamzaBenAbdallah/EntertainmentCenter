543c147e07a1004ff87151802cb2ed63849f77d4
Hamza <hamza.ben.abdallah02@gmail.com>
2022-08-31 19:00:11 -0400

    Create a Carousel card component

---
+++ client/src/Components/CarouselCard/CarouselCard.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  cursor: pointer;
+  position: relative;
+  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.35);
+  transition: all 0.3s ease;
+  height: max-content;
+`;
+
+export const Poster = styled.img`
+  display: block;
+  width: 100%;
+`;
