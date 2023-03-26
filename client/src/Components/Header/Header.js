1e9b6e59778ade31e84d28338c3b91738596a3a5
Hamza <hamza.ben.abdallah02@gmail.com>
2023-03-26 17:20:59 -0400

    Update the title and the logo

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -3,57 +3,61 @@ import { getCurrentUser } from "Services/getCurrentUser";
 import SearchBar from "Components/SearchBar";
 import { Icon } from "@iconify/react";
 import CE from "Pictures/CE.png";
+import logo from "Pictures/Logo.png";
 import {
-  Head,
-  Wrapper,
-  Container,
-  Logo,
-  Button,
-  Page,
-  Link,
+    Head,
+    Wrapper,
+    Container,
+    Logo,
+    Button,
+    Page,
+    Link,
 } from "./Header.style";
 
 const Header = () => {
-  const { user } = getCurrentUser();
-  const { handleLogout } = useLogout();
-  return (
-    <Head>
-      <Wrapper>
-        <Container>
-          <Link to="">
-            <Logo>
-              <img src={CE} alt="EC" />
-            </Logo>
-          </Link>
-          <Link to="Discover">
-            <Page>Discover</Page>
-          </Link>
-        </Container>
-        <Container>
-          <SearchBar />
-          {!user ? (
-            <>
-              <Link to="login">
-                <Button>Login</Button>
-              </Link>
-              <Link to="signup">
-                <Button>Signup</Button>
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link to="profile">
-                <Icon icon="healthicons:ui-user-profile" width="3rem" />
-              </Link>
-              <Link to="" onClick={handleLogout}>
-                <Button>Logout</Button>
-              </Link>
-            </>
-          )}
-        </Container>
-      </Wrapper>
-    </Head>
-  );
+    const { user } = getCurrentUser();
+    const { handleLogout } = useLogout();
+    return (
+        <Head>
+            <Wrapper>
+                <Container>
+                    <Link to="">
+                        <Logo>
+                            <img src={logo} alt="EC" />
+                        </Logo>
+                    </Link>
+                    <Link to="Discover">
+                        <Page>Discover</Page>
+                    </Link>
+                </Container>
+                <Container>
+                    <SearchBar />
+                    {!user ? (
+                        <>
+                            <Link to="login">
+                                <Button>Login</Button>
+                            </Link>
+                            <Link to="signup">
+                                <Button>Signup</Button>
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="profile">
+                                <Icon
+                                    icon="healthicons:ui-user-profile"
+                                    width="3rem"
+                                />
+                            </Link>
+                            <Link to="" onClick={handleLogout}>
+                                <Button>Logout</Button>
+                            </Link>
+                        </>
+                    )}
+                </Container>
+            </Wrapper>
+        </Head>
+    );
 };
 
 export default Header;
--- client/src/Components/Header/Header.style.js
+++ client/src/Components/Header/Header.style.js
@@ -2,50 +2,50 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const Head = styled.header`
-  display: flex;
-  justify-content: center;
-  background-color: #032541;
-  border-bottom: 2px solid #000;
-  height: 5rem;
+    display: flex;
+    justify-content: center;
+    background-color: #032541;
+    border-bottom: 2px solid #000;
+    height: 5rem;
 `;
 
 export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  color: white;
-  width: 80%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    color: white;
+    width: 80%;
 `;
 
 export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
 `;
 
 export const Logo = styled.div`
-  img {
-    width: 8rem;
-  }
+    img {
+        width: 4.5rem;
+    }
 `;
 
 export const Page = styled.div`
-  font-size: 1.25em;
+    font-size: 1.25em;
 `;
 
 export const Button = styled.div`
-  font-size: 1.1em;
-  border: 2px solid #abb7c4;
-  padding: 6px 15px;
-  border-radius: 30px;
+    font-size: 1.1em;
+    border: 2px solid #abb7c4;
+    padding: 6px 15px;
+    border-radius: 30px;
 
-  &:hover {
-    border-color: #fff;
-  }
+    &:hover {
+        border-color: #fff;
+    }
 `;
 
 export const Link = styled(NavLink)`
-  color: inherit;
-  text-decoration: inherit;
-  display: contents;
+    color: inherit;
+    text-decoration: inherit;
+    display: contents;
 `;
